99bea201c8cb29676777b6b5a365fa6384d29bf7
Vesko Kenashkov <vesko@azonmedia.com>
2020-03-10 17:14:59 +0200

    fixed PostInstall

--- app/src/PostInstall.php
+++ app/src/PostInstall.php
@@ -12,14 +12,15 @@ class PostInstall implements PostInstallHookInterface
 {
     public static function post_install_hook(Installer $Installer, InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
     {
-        $guzaba_platform_dir = $Installer->getInstallPath($Package);
-        //$private_assets_dir = $guzaba_platform_dir . '/app/assets';
+        $package_install_dir = $Installer->getInstallPath($Package);
+        $guzaba_platform_dir = $package_install_dir.'/../../../';
         $public_assets_dir = $guzaba_platform_dir . '/app/public/assets';
-        print 'Creating public assets dir: '.$public_assets_dir.PHP_EOL;
+        //print 'Creating public assets dir: '.$public_assets_dir.PHP_EOL;
 //        if (!file_exists($private_assets_dir)) {
 //            mkdir($private_assets_dir);
 //        }
         if (!file_exists($public_assets_dir)) {
+            print 'Creating public assets dir: '.$public_assets_dir.PHP_EOL;
             mkdir($public_assets_dir);
         }
     }
